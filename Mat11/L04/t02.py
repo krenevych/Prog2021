a8a69a00bc07d281ffa28a7c8bc857e9e6f72ff2
Andrii Krenevych <akrenevych@gmail.com>
2021-09-24 09:10:37 +0300

    2

---
+++ Mat11/L04/t02.py
@@ -0,0 +1,21 @@
+n = int(input()) # номер варіанту
+
+# 10 .. 99
+res = 0
+for i in range(10, 100):
+    od = i % 10 # одиниці
+    de = i // 10 # десятки
+    if n == 1:
+        if de > od: res += 1
+    elif n == 2:
+        if de < od: res += 1
+    elif n == 3:
+        if (de + od) % 2 == 0: res += 1
+    elif n == 4:
+        if de == od: res += i
+    elif n == 5:
+        if de % 2 == od % 2 == 0: res += i
+    elif n == 6:
+        if de % 2 == od % 2 == 1: res += i
+
+print(res)
