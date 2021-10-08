b6fbdbdc3279a922fb57d7ffab6ee397f0f9a4bd
Andrii Krenevych <akrenevych@gmail.com>
2021-10-08 09:59:02 +0300

    22

---
+++ Mat22/L06/t04.py
@@ -0,0 +1,27 @@
+# "     Alexandr    Sergeevich   Pushkin      "
+# "Alexandr Sergeevich Pushkin"
+
+# s = "     Alexandr    Sergeevich   Pushkin      "
+s = input()
+print(" ".join(s.split()))
+
+# new_s = s[0]
+# for ch in s[1:]:
+#     if ch != " ":
+#         new_s += ch
+#     else:
+#         last_in_new_s = new_s[-1]
+#         if last_in_new_s != " ":
+#             new_s += " "
+#
+# if new_s[0] == " ":
+#     new_s = new_s[1:]
+#
+# if new_s[-1] == " ":
+#     new_s = new_s[:-1]
+
+# print(new_s)
+
+
+
+
