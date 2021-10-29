9df156014fe0a53e24fab038516e11fc6779bbca
Andrii Krenevych <akrenevych@gmail.com>
2021-10-29 09:53:28 +0300

    22

---
+++ Mat11/L08/t04.py
@@ -0,0 +1,37 @@
+# s = "asdffdsa"
+# isSym(s) = (перший символ рядка s == останній символ рядка s) і isSym(s без першого і останнього)
+
+# прямий хід рекурсії
+# isSym("asdffdsa") = "a" == "a" and isSym("sdffds")
+#                                        ||
+#                                    "s" == "s"  and  isSym("dffd")
+#                                                         ||
+#                                                      "d" == "d"  and  isSym("ff")
+#                                                                          ||
+#                                                                       "f" == "f"  and  isSym("")
+#                                                                                          ||
+#                                                                                         True
+
+# зворотній хід рекурсії
+# isSym("asdffdsa") = "a" == "a" and True = True
+#                                        ||
+#                                    "s" == "s"  and  True = True
+#                                                         ||
+#                                                      "d" == "d"  and  True = True
+#                                                                          ||
+#                                                                       "f" == "f"  and  True = True
+#                                                                                          ||
+#                                                                                         True
+
+
+# isSym("asdffdEa") = "a" == "a" and False = False
+#                                        ||
+#                                    "s" == "E"  and  isSym("dffd") = False
+
+def isSym(s):
+    if len(s) <= 1:
+        return True
+    else:
+        return s[0] == s[-1] and isSym(s[1:-1])
+
+print(isSym("asdffdsa"))
