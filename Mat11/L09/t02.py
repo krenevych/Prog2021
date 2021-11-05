1e5319ad6b8ad2c38c33a4c3252c0409d9992d93
Andrii Krenevych <akrenevych@gmail.com>
2021-11-05 09:09:17 +0200

    22

---
+++ Mat11/L09/t02.py
@@ -0,0 +1,16 @@
+n = int(input())
+array = [int(el) for el in input().split()]
+
+freq = {}
+for a in array:
+    if a in freq:
+        freq[a] += 1
+    else:
+        freq[a] = 1
+
+for key in freq:
+    if freq[key] > n // 2:
+        print(key)
+        break
+else:
+    print(-1)
