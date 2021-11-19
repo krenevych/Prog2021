0bc53c3e2d1bb5546277cfb277565fab5df17b7c
Andrii Krenevych <akrenevych@gmail.com>
2021-11-19 09:32:45 +0200

    22

--- Mat11/L10/t06.py
+++ Mat11/L10/t06.py
@@ -1,18 +1,4 @@
-# from base import printMatrix, readMatrix
-def readMatrix(n, typeElements=int):
-    M = []
-    for i in range(n):
-        row = [typeElements(el) for el in input().split()]
-        M.append(row)
-        # print("row %d" % i, row)
-    return M
-
-def printMatrix(M, sep="", format="%4d"):
-    for row in M:
-        for el in row:
-            print(format % el, end=sep)
-        print()
-
+from base import printMatrix, readMatrix
 
 def multMatrix(a, b):
     row_c = len(a)
