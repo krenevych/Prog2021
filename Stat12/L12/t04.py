30145ca50301e322fdc3298d92fd0b754aaa27a8
Andrii Krenevych <akrenevych@gmail.com>
2021-12-02 12:10:23 +0200

    222

--- Stat12/L12/t04.py
+++ Stat12/L12/t04.py
@@ -1,8 +1,21 @@
 def sqr_a(x):
-    return 0
+    assert x >= 0
+    eps = 0.000001
+    a = x / 2
+    while True:
+        a_prev = a
+        a = 0.5 * (a + x / a)
+        if abs(a - a_prev) < eps:
+            break
+
+        # print(x, a, a_prev, abs(a - a_prev))
+
+    return a
+
 
 
 def sqr_b(x):
+    assert x >= 0
     eps = 0.000001
     a = x / 2
     while abs(a**2 - x) >= eps:
@@ -13,7 +26,7 @@ def sqr_b(x):
 
 
 if __name__ == "__main__":
-    x = 13
+    x = 14
     print(f"sqr_a({x}) =", sqr_a(x))
     print(f"sqr_b({x}) =", sqr_b(x))
     print(f"  {x}**0.5 =", x ** 0.5)
