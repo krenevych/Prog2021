30dd1a91e5a4892ed2c9312d03773e90b638c1ea
Andrii Krenevych <krenevych@knu.ua>
2022-01-25 11:49:53 +0200

    3

--- Stat12/L13/file_lib.py
+++ Stat12/L13/file_lib.py
@@ -7,6 +7,18 @@ def calculateSum(fname):
     f.close()
     return suma
 
+def countNeg(fname):
+    counter = 0
+    f = open(fname)
+    for line in f:
+        a = [float(el) for el in line.split()]
+        for el in a:
+            if el < 0:
+                counter += 1
+    f.close()
+    return counter
+
 
 if __name__ == "__main__":
-    print(calculateSum("input13_2.txt"))
+    # print(calculateSum("input13_2.txt"))
+    print(countNeg("input13_2.txt"))
