52775d72583a8b9b32fdf6d9c11876ffff38aa12
Andrii Krenevych <krenevych@knu.ua>
2022-01-27 10:13:31 +0200

    111

--- Mat11/L13/t03.py
+++ Mat11/L13/t03.py
@@ -1,12 +1,11 @@
-def myprint(*args, **kwargs):
-    print(*args)
-    print(*args, **kwargs)
+# def myprint(*args, **kwargs):
+#     print(*args, **kwargs)
+#     del kwargs['file']
+#     print(*args, **kwargs)
 
 
 f = open("output.txt", "w")
 for i in range(1, 10):
     # myprint(str(i) * i, file=f)
     print(str(i) * i, file=f)
-
-
 f.close()
