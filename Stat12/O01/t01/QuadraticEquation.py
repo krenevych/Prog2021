1e94c345f48a45f911355138944d0894e3ae1ee1
Andrii Krenevych <akrenevych@gmail.com>
2022-02-08 11:43:25 +0200

    Конструктор копіювання для квадратного рівнняння

--- Stat12/O01/t01/QuadraticEquation.py
+++ Stat12/O01/t01/QuadraticEquation.py
@@ -1,8 +1,13 @@
 class QuadraticEquation:
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+    def __init__(self, a, b=None, c=None):
+        if isinstance(a, QuadraticEquation):
+            self.a = a.a
+            self.b = a.b
+            self.c = a.c
+        else:
+            self.a = a
+            self.b = b
+            self.c = c
 
     def show(self):
         print(f"{self.a}x^2 + {self.b}x + {self.c} = 0")
@@ -35,26 +40,35 @@ class QuadraticEquation:
 
 
 if __name__ == "__main__":
+    # eq1 = QuadraticEquation(1, 2, 1)
+    # eq1.show()
+    # print(eq1.solutions())
+    #
+    # eq0 = QuadraticEquation(1, -3, 3)
+    # eq0.show()
+    # print(eq0.solutions())
+    #
+    # eq2 = QuadraticEquation(1, -3, 2)
+    # eq2.show()
+    # print(eq2.solutions())
+    #
+    # linear0 = QuadraticEquation(0, 0, 3)
+    # linear0.show()
+    # print(linear0.solutions())
+    # linear1 = QuadraticEquation(0, 1, 3)
+    # linear1.show()
+    # print(linear1.solutions())
+    # linear_inf = QuadraticEquation(0, 0, 0)
+    # linear_inf.show()
+    # print(linear_inf.solutions())
+
     eq1 = QuadraticEquation(1, 2, 1)
     eq1.show()
     print(eq1.solutions())
 
-    eq0 = QuadraticEquation(1, -3, 3)
-    eq0.show()
-    print(eq0.solutions())
-
-    eq2 = QuadraticEquation(1, -3, 2)
+    eq2 = QuadraticEquation(eq1)
+    eq2.a = -100
     eq2.show()
     print(eq2.solutions())
-
-    linear0 = QuadraticEquation(0, 0, 3)
-    linear0.show()
-    print(linear0.solutions())
-    linear1 = QuadraticEquation(0, 1, 3)
-    linear1.show()
-    print(linear1.solutions())
-    linear_inf = QuadraticEquation(0, 0, 0)
-    linear_inf.show()
-    print(linear_inf.solutions())
 
 
