18422ef06d10cf6a471b472eba4bfc075d24ed6b
Andrii Krenevych <akrenevych@gmail.com>
2022-02-10 09:46:00 +0200

    222

--- Mat11/O01/t01/QuadraticEquation.py
+++ Mat11/O01/t01/QuadraticEquation.py
@@ -1,4 +1,5 @@
 class QuadraticEquation:
+
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
@@ -12,7 +13,14 @@ class QuadraticEquation:
 
     def solutions(self):
         if self.a == 0:  # bx + c = 0
-            pass
+            if self.b == 0: # c = 0
+                if self.c == 0:  # 0 = 0
+                    return "INF"
+                else:           # 5 = 0
+                    return ()
+            else:
+                x = -self.c / self.b
+                return x,
         else:  # ax^2 + bx + c = 0
             d = self.discriminant()
             if d < 0:
@@ -28,14 +36,33 @@ class QuadraticEquation:
 
 
 if __name__ == "__main__":
-    eq0 = QuadraticEquation(1, -4, 14)
+    # a1 = int(input("a="))
+    # b2 = int(input("b="))
+    # c3 = int(input("c="))
+    # eq = QuadraticEquation(a1, b2, c3)
+    # eq.show()
+    # print(eq.solutions())
+
+    # eq0 = QuadraticEquation(1, -4, 14)
+    # eq0.show()
+    # print(eq0.solutions())
+    #
+    # eq1 = QuadraticEquation(1, -4, 4)
+    # eq1.show()
+    # print(eq1.solutions())
+    #
+    # eq2 = QuadraticEquation(1, -3, 2)
+    # eq2.show()
+    # print(eq2.solutions())
+
+    eq0 = QuadraticEquation(0, 0, 14)
     eq0.show()
     print(eq0.solutions())
 
-    eq1 = QuadraticEquation(1, -4, 4)
+    eq1 = QuadraticEquation(0, 1, 14)
     eq1.show()
     print(eq1.solutions())
 
-    eq2 = QuadraticEquation(1, -3, 2)
-    eq2.show()
-    print(eq2.solutions())
+    eq_inf = QuadraticEquation(0, 0, 0)
+    eq_inf.show()
+    print(eq_inf.solutions())
