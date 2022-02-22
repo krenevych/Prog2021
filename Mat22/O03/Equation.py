c6c35cec049b6291aa824fb9f765f82e4910b0b1
Andrii Krenevych <akrenevych@gmail.com>
2022-02-22 09:25:46 +0200

    2222

--- Mat22/O03/Equation.py
+++ Mat22/O03/Equation.py
@@ -1,22 +1,22 @@
 class Equation:
 
     def __init__(self, b, c):
-        self.__b = b
-        self.__c = c
+        self._b = b
+        self._c = c
 
     def __str__(self):
-        sign_c = "+" if self.__c >= 0 else "-" if self.__c < 0 else ""
-        res = f"{self.__b}x {sign_c} {abs(self.__c)} = 0"
+        sign_c = "+" if self._c >= 0 else "-" if self._c < 0 else ""
+        res = f"{self._b}x {sign_c} {abs(self._c)} = 0"
         return res
 
     def solve(self):
-        if self.__b == 0:  # c = 0
-            if self.__c == 0:  # 0 = 0
+        if self._b == 0:  # c = 0
+            if self._c == 0:  # 0 = 0
                 return "inf"
             else:
                 return ()
         else:  # bx + c = 0
-            x = -self.__c / self.__b
+            x = -self._c / self._b
             return x,
 
 
---
+++ Mat22/O03/QuadraticEquation.py
@@ -0,0 +1,58 @@
+from Equation import Equation
+
+
+class QuadraticEquation(Equation):
+    def __init__(self, a, b, c):
+        super().__init__(b, c)
+        self._a = a
+
+    def __str__(self):
+        res = f"{self._a}x^2 + "
+        res += super().__str__()
+        return res
+
+    def discrimionant(self):
+        d = self._b ** 2 - 4 * self._a * self._c
+        return d
+
+    def solve(self):
+        if self._a == 0:  # bx + c = 0
+            return super().solve()
+        else:  # ax^2 + bx + c = 0
+            d = self.discrimionant()
+            if d < 0:
+                return ()
+            elif d == 0:
+                x = -self._b / (2 * self._a)
+                return x,
+            else:  # d > 0
+                d2 = d ** 0.5
+                x1 = (-self._b + d2) / (2 * self._a)
+                x2 = (-self._b - d2) / (2 * self._a)
+                return (x1, x2)
+
+
+if __name__ == '__main__':
+    eq = QuadraticEquation(0, 0, 0)
+    print(eq)
+    print(eq.solve())
+
+    eq = QuadraticEquation(0, 0, -2)
+    print(eq)
+    print(eq.solve())
+
+    eq = QuadraticEquation(0, 3, -2)
+    print(eq)
+    print(eq.solve())
+
+    eq2 = QuadraticEquation(1, -3, 2)
+    print(eq2)
+    print(eq2.solve())
+
+    eq2 = QuadraticEquation(1, -4, 4)
+    print(eq2)
+    print(eq2.solve())
+
+    eq2 = QuadraticEquation(1, -4, 19)
+    print(eq2)
+    print(eq2.solve())
