6308c22b7b41045376244f53001766d14e1e00d0
Andrii Krenevych <akrenevych@gmail.com>
2022-02-22 11:43:14 +0200

    333

--- Stat12/O03/BiQuadraticEquation.py
+++ Stat12/O03/BiQuadraticEquation.py
@@ -1,4 +1,5 @@
 from QuadraticEquation import QuadraticEquation
+from Equation import Equation
 
 
 class BiQuadraticEquation(QuadraticEquation):
@@ -6,8 +7,34 @@ class BiQuadraticEquation(QuadraticEquation):
     def __str__(self):
         return f"{self._a}x^4 + {self._b}x^2 + {self._c} = 0"
 
-    
+    def solve(self):
+        solutions_quadratic = super().solve()
+        if solutions_quadratic == Equation.INF:
+            return solutions_quadratic
+        else:
+            solutions = set()
+            for sol_quad in solutions_quadratic:
+                if sol_quad == 0:
+                    solutions.add(0)
+                elif sol_quad > 0:
+                    x = sol_quad ** 0.5
+                    solutions.add(x)
+                    solutions.add(-x)
+
+            # solutions = list(solutions)
+            # solutions.sort()
+            return tuple(solutions)
+
 
 if __name__ == '__main__':
-    eq4 = BiQuadraticEquation(2, 8, 8)
+    eq4 = BiQuadraticEquation(0, 0, 0)
     print(eq4)
+    print(eq4.solve())
+
+    eq4 = BiQuadraticEquation(1, -5, 4)
+    print(eq4)
+    print(eq4.solve())
+
+    eq4 = BiQuadraticEquation(1, -4, 0)
+    print(eq4)
+    print(eq4.solve())
